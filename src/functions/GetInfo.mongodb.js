617c44ed7e3000ca0a4fb54e548f4a732f20db4e
CaioMaile <caiomailesla@gmail.com>
2024-12-03 15:34:13 -0300

    playground mongo

--- src/functions/GetInfo.mongodb.js
+++ src/functions/GetInfo.mongodb.js
@@ -1,4 +1,16 @@
 use("test")
 const Eu = db.getCollectionNames()
+
+const infor = db.asd.aggregate([
+    {
+        $match: { nome: "caio"}
+    },
+    {
+        $group: {_id: "$mensagem"}
+    }
+])
+
 console.log(Eu)
+console.log("E")
+console.log(infor)
 
